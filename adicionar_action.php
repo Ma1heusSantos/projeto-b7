86d85fe6c40a9849c698faa6314993bae437d81a
Olá mundo, Meu nome é Matheus Santos <93716685+Ma1heusSantos@users.noreply.github.com>
2023-01-26 15:48:23 -0300

    iniciando crud com banco de dados

--- adicionar_action.php
+++ adicionar_action.php
@@ -2,15 +2,20 @@
 require "config.php";
 
 $name = filter_input(INPUT_POST,"name");
-$email = filter_input(INPUT_POST,"email",FILTER_VALIDATE_EMAIL);
+$email = filter_input(INPUT_POST,"email", FILTER_VALIDATE_EMAIL);
 
 if($name && $email){
 
-    $sql = $pdo -> prepare("INSERT INTO usuarios (nome,email) VALUES (:name :email)");
-    $sql
+    $sql = $pdo -> prepare("INSERT INTO usuarios (nome, email) VALUES (:name :email)");
+    $sql->bindValue(':name',$name);
+    $sql->bindValue(':email,',$email);
+    $sql->execute();
+
+    header("Location: index.php");
+    exit;
 
 }else{
-    header("location: adicionar.php");
+    header("Location: adicionar.php");
     exit;
 }
 ?>
